cdf1ba1e4dd31ec09e00f5afd1b761d289764086
Ayush Kaithwas <ayush.91011@gmail.com>
2024-02-11 15:20:44 +0530

    email added

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,8 @@
 import { Container } from "@/components/ui-self/container/container";
 import { H1 } from "@/components/ui-self/heading";
-import { Phone } from "lucide-react";
+import { Mail, Phone } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Page() {
   return (
@@ -14,7 +15,7 @@ export default function Page() {
           height={800}
           className="hidden rounded-lg md:inline md:w-[50rem]"
         />
-        <div className="flex w-full flex-col items-center gap-3 rounded-lg border-2 py-10 text-center md:w-[50rem] md:px-[10rem] md:py-[5rem]">
+        <div className="flex w-full flex-col items-center justify-center gap-3 rounded-lg border-2 py-10 text-center md:w-[50rem] md:px-[10rem] md:py-[5rem]">
           <H1>Prof. Manabendra Dutta Choudhury</H1>
           <h1 className="text-lg md:text-2xl">
             Director, Planning & Event Management Son Beel Utsav 2024
@@ -33,7 +34,7 @@ export default function Page() {
           height={800}
           className="hidden rounded-lg md:inline md:w-[50rem]"
         />
-        <div className="flex w-full flex-col items-center gap-3 rounded-lg border-2 py-10 text-center md:w-[50rem] md:px-[10rem] md:py-[5rem]">
+        <div className="flex w-full flex-col items-center justify-center gap-3 rounded-lg border-2 py-10 text-center md:w-[50rem] md:px-[10rem] md:py-[5rem]">
           <H1>Dr. Arun Jyoti Nath</H1>
           <h1 className="text-lg md:text-2xl">
             Task Force, Convener, Son Beel Utsav 2024
@@ -42,6 +43,27 @@ export default function Page() {
             <Phone size={20} />
             <p className="text-lg font-bold md:text-2xl">+91 7636023026</p>
           </div>
+        </div>
+      </div>
+      <div className="flex w-full justify-center gap-10">
+        <Image
+          src="/images/gallery-images/4.png"
+          alt="logo"
+          width={800}
+          height={800}
+          className="hidden rounded-lg md:inline md:w-[50rem]"
+        />
+        <div className="flex w-full flex-col items-center justify-center gap-3 rounded-lg border-2 py-10 text-center md:w-[50rem] md:px-[10rem] md:py-[5rem]">
+          <H1>Email</H1>
+          <Link
+            className="flex items-center justify-center gap-1 md:gap-3"
+            href="mailto:utsabsonbeel@gmail.com"
+          >
+            <Mail />
+            <p className="text-lg font-bold md:text-2xl">
+              utsabsonbeel@gmail.com
+            </p>
+          </Link>
         </div>
       </div>
     </Container>
