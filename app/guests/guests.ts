6081e3f12d7d10259ebb54c32c52f2e5b2797d47
Ayush Kaithwas <ayush.91011@gmail.com>
2024-02-11 15:22:27 +0530

    guest list added

---
+++ app/guests/guests.ts
@@ -0,0 +1,68 @@
+type invitedGuest = {
+  name: string;
+  pic: string | null;
+  url: string | null;
+};
+
+const invitedGuests: invitedGuest[] = [
+  {
+    name: "Amar Mitra",
+    pic: "/images/amar-mitra.jpg",
+    url: "https://en.wikipedia.org/wiki/Amar_Mitra_(writer)",
+  },
+  {
+    name: "Sadhan Chattopadhyay",
+    pic: "/images/sadhan-chattopadhyay.jpg",
+    url: null,
+  },
+  {
+    name: "Arun Kr Roy",
+    pic: null,
+    url: null,
+  },
+  {
+    name: "Falguni Dey",
+    pic: null,
+    url: null,
+  },
+  {
+    name: "Ratnadeep Choudhury, NDTV",
+    pic: null,
+    url: null,
+  },
+  {
+    name: "Chandrani Sinha, Russia TV",
+    pic: null,
+    url: null,
+  },
+  {
+    name: "P J Barua, executive Editor, Assam Tribune",
+    pic: null,
+    url: null,
+  },
+  {
+    name: "Padmasree Anup Sah, Nainital",
+    pic: null,
+    url: null,
+  },
+  {
+    name: "V S Ohri, Delhi",
+    pic: null,
+    url: null,
+  },
+  {
+    name: "S S Rasaily, Dehradun",
+    pic: null,
+    url: null,
+  },
+  {
+    name: "Tulika Mondal and Gangadhar Mondal and team from Zee Bangla",
+    pic: null,
+    url: null,
+  },
+  {
+    name: "Baul team from Shantiniketan, Tripura, and Silchar",
+    pic: null,
+    url: null,
+  },
+];
