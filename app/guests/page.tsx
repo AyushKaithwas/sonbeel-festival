bc162f10a87f71b8e8669f87e2d448bb44ec9903
Ayush Kaithwas <ayush.91011@gmail.com>
2024-02-11 15:52:44 +0530

    guest list implementation

--- app/guests/page.tsx
+++ app/guests/page.tsx
@@ -1,9 +1,44 @@
 import { Container } from "@/components/ui-self/container/container";
+import { invitedGuests } from "./guests";
+import Image from "next/image";
+import { H1 } from "@/components/ui-self/heading";
 
 export default function Page() {
   return (
     <Container>
-      <h1>Guests</h1>
+      <H1 className="text-center">Guests</H1>
+      {invitedGuests.map((guest) => {
+        return (
+          <div
+            className="flex h-[14rem] w-[50rem] items-center justify-start gap-10 rounded-3xl border border-primary px-10"
+            key={guest.name}
+          >
+            <Image
+              src={guest.pic ? guest.pic : "/images/guest/default.jpg"}
+              alt={guest.name}
+              width={200}
+              height={200}
+              className="w-[10rem] rounded-3xl"
+            />
+            <div>
+              <h1 className="text-2xl font-semibold text-secondary">
+                {guest.name}
+              </h1>
+
+              {guest.url && (
+                <a
+                  className="hover:text-primary"
+                  href={guest.url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Website
+                </a>
+              )}
+            </div>
+          </div>
+        );
+      })}
     </Container>
   );
 }
